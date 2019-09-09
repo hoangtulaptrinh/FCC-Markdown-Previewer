c23bbd87125adf9ec3b0e5eb752fe40a4ec9102e
Your Name <you@example.com>
2019-09-09 20:31:16 +0700

    Updates

---
+++ precache-manifest.ec181b047760e216ce8e701e2c6e37ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c403fe7ef6f39fd2a08b325ed43e5cb3",
+    "url": "/FCC-Markdown-Previewer/index.html"
+  },
+  {
+    "revision": "702818f6be60c2502c12",
+    "url": "/FCC-Markdown-Previewer/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "f5c021c802a0cc7d910e",
+    "url": "/FCC-Markdown-Previewer/static/css/main.ee3df343.chunk.css"
+  },
+  {
+    "revision": "702818f6be60c2502c12",
+    "url": "/FCC-Markdown-Previewer/static/js/2.173177a0.chunk.js"
+  },
+  {
+    "revision": "f5c021c802a0cc7d910e",
+    "url": "/FCC-Markdown-Previewer/static/js/main.b31f8a88.chunk.js"
+  },
+  {
+    "revision": "18a2db55cd6a06e80379",
+    "url": "/FCC-Markdown-Previewer/static/js/runtime~main.672e1c9e.js"
+  },
+  {
+    "revision": "0b000b0b24ae2767c12eec966024c617",
+    "url": "/FCC-Markdown-Previewer/static/media/2a459f043ed0863828f4fb0754b8976e.0b000b0b.jpg"
+  }
+]);
